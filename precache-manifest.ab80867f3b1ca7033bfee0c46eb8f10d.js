af44fd7a11219b3925e4251589d68dae8bb7e07b
Trần Thị Thủy Tiên <71958178+thuytien1510@users.noreply.github.com>
2021-01-03 15:30:26 +0700

    Add files via upload

---
+++ precache-manifest.ab80867f3b1ca7033bfee0c46eb8f10d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e398f3d927e97d90efe9e5773e648a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e2d683edd327d7a9beb",
+    "url": "/static/js/2.6113de88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6113de88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ddd6367c744733d7e53",
+    "url": "/static/js/main.2e5849e6.chunk.js"
+  },
+  {
+    "revision": "ba7c76c412aa89adf6ed",
+    "url": "/static/js/runtime-main.11ba2872.js"
+  }
+]);
